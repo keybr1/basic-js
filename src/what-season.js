5f571db7a36c4b3b39ce74bc01bed3043032f17b
unknown <sadmin@W10-NOVOSIB.smb.net>
2020-11-02 01:12:37 +0700

    feat: task what season ready

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getseason(date) {
+  if(!date){
+    return 'Unable to determine the time of year!';
+  };
+
+  if(!date.getTime()){
+    throw new Error();
+  }
+
+  let season = '';
+  const month = date.getMonth();
+
+  if(month === 0 || month === 1 || month === 11){
+    return season = 'winter'
+  } else if(month >= 2 && month <= 4) {
+    return season = 'spring'
+  } else if(month >= 5 && month <= 7) {
+    return season = 'summer'
+  } else if(month >= 8 && month <= 10) {
+    return season = 'autumn'
+  }
 };
